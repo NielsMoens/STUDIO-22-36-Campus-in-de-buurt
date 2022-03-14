eb9e0e9a568314160242227bcecbddc5b9e09b9d
pgmgent-kobedevi <kobedevi@student.arteveldehs.be>
2022-03-14 01:28:07 +0100

    added redirect to logout

--- maps-db-app/src/components/Auth/LogoutButton.js
+++ maps-db-app/src/components/Auth/LogoutButton.js
@@ -1,10 +1,12 @@
 import Button from '../Design/Button';
 import { useAuth } from './AuthContainer';
+import { Link } from 'react-router-dom';
+
 
 const LogoutButton = () => {
     const { logout } = useAuth();
 
-    return <Button onClick={logout} color="outline-light">Sign out </Button>
+    return <Link to="/splash"><Button onClick={logout} color="outline-light">Sign out </Button></Link>
 } 
 
 export default LogoutButton;
