508917ef61f2de1a33726dcd6d0d6df2c78f1fa4
pgmgent-kobedevi <kobedevi@student.arteveldehs.be>
2022-03-28 17:54:36 +0200

    removed unused functions

--- maps-db-api/controllers/MarkerController.js
+++ maps-db-api/controllers/MarkerController.js
@@ -40,65 +40,6 @@ class MarkerController {
         }
     }
 
-    // getMoviesByDirectorId = async(req, res, next) => {
-    //     try {
-    //         const {id} = req.params;
-    //         const movies = await Movie.find({directorId: id}).exec();
-    //         if(movies) {
-    //             res.status(200).json(movies);
-    //         } else {
-    //             next(new NotFoundError());
-    //         }
-    //     } catch(e) {
-    //         next(e);
-    //     }
-    // }
-
-    // deleteDirectorById = async(req,res,next) => {
-    //     try {
-    //         const {id} = req.params;
-    //         const director = await Director.findById(id).exec();
-    //         if(director) {
-    //             // cant use pre because off multiple delete possibilities
-    //             await Movie.find({directorId: id}).exec()
-    //             .then( async(res) => {
-    //                 res.map( async(movie) => {
-    //                     movie.directorId = null;
-    //                     await movie.save();
-    //                 })
-    //             })
-    //             .then(async () => await director.remove())
-    //             res.status(200).json({director});
-    //         } else {
-    //             next(new NotFoundError());
-    //         }
-    //     } catch(e) {
-    //         next(e);
-    //     }
-    // }
-
-    // deleteDirectorByIdAndMovies = async(req,res,next) => {
-    //     try {
-    //         const {id} = req.params;
-    //         const director = await Director.findById(id).exec();
-    //         if(director) {
-    //             // cant use pre because off multiple delete possibilities
-    //             await Movie.find({directorId: id}).exec()
-    //             .then( async(res) => {
-    //                 res.map( async(movie) => {
-    //                     await movie.remove()
-    //                 })
-    //             })
-    //             .then(async () => await director.remove())
-    //             res.status(200).json({director});
-    //         } else {
-    //             next(new NotFoundError());
-    //         }
-    //     } catch(e) {
-    //         next(e);
-    //     }
-    // }
-
     updateMarkerById = async(req,res,next) => {
         try {
             const {id} = req.params;
